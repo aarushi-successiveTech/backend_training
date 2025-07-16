1a3c0c1a8328c8966316030490c65c0736e89f92
Aarushi Mahajan <aarushi.mahajan@aarushivrs-24.successivetech.com>
2025-07-16 18:04:06 +0530

    dist untracked from git

--- assignment3/dist/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUser = void 0;
-const express_1 = require("express");
-const getUser = (req, res) => {
-    express_1.response.status(200).json({ message: 'user data fetched' });
-};
-exports.getUser = getUser;
--- assignment3/dist/mockData.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const users = [
-    { id: 1, name: "Aarushi", email: "aarushi@mail.com" },
-    { id: 2, name: "eklavya", email: "eklavya@mail.com" },
-    { id: 3, name: "akhil", email: "akhil@mail.com" },
-];
-module.exports = users;
