4578790903de35b76f9f0e6764f3918c45bd61d2
Aarushi Mahajan <aarushi.mahajan@aarushivrs-24.successivetech.com>
2025-07-16 18:01:44 +0530

    dist untracked from git

--- assignment2/dist/mockData.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const users = [
-    { id: 1, name: 'Aarushi' },
-    { id: 2, name: 'Vaishnavi' },
-    { id: 3, name: 'eklavya' },
-    { id: 4, name: 'Akhil' }
-];
-module.exports = users;
