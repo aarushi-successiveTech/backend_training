6377489d2d81c0a5bbd264c513d04c4fb2360f0a
Aarushi Mahajan <aarushi.mahajan@aarushivrs-24.successivetech.com>
2025-07-20 12:28:48 +0530

    removed dist

--- assignment5/dist/middleware/loggerMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loggerMiddleware = void 0;
-const loggerMiddleware = (req, res, next) => {
-    const timeStamp = new Date().toLocaleTimeString();
-    console.log(`current time ${timeStamp}, ${req.method} and ${req.originalUrl}`);
-    next();
-};
-exports.loggerMiddleware = loggerMiddleware;
